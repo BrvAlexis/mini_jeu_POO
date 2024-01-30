0eca78b056311c43a8fef11e9ba297a06ff33b65
BrvAlexis <brevieralexis@gmail.com>
2024-01-30 11:43:54 +0100

    f)

--- app.rb
+++ app.rb
@@ -4,12 +4,46 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-player1 = Player.new("José", 10)
+#À ma droite "Josiane" : crée un Player répondant à ce doux prénom et stocké dans la variable player1
+player1 = Player.new("Josiane",10)
 
-player1.gets_damage(5)  # Player José takes 5 damage
-player1.show_state      # Output: José a 5 points de vie
+#À ma gauche "José" : crée un autre Player répondant à ce joli prénom et stocké dans la variable player2
+player2 = Player.new("José",10)
+
+#Présentons les deux combattants : affiche dans le terminal l'état de chaque combattant grâce à des puts et des show_state. 
+#Juste avant, affiche un petit puts "Voici l'état de chaque joueur :".
+
+puts "Voici l'état de chaque joueur :"
+player1.show_state
+player2.show_state
+
+#Fight ! Indique que le combat commence avec un puts "Passons à la phase d'attaque :"
+puts "Passons à la phase d'attaque :"
 
 
 
-binding.pry
+while player1.life_points > 0 && player2.life_points > 0
+    #Josiane aura l'honneur d'attaquer la première : 
+#fais attaquer player2 par player1 avec la méthode attacks.
+    player1.attacks(player2)
+    player2.show_state
+  
+    # Vérifie si player2 est toujours en vie avant de le laisser attaquer
+    if player2.life_points <= 0
+      puts "#{player2.name} a été tué !"
+      break
+    end
+  
+    player2.attacks(player1)
+    player1.show_state
+  end
+  
+
+
+puts "Le combat est terminé !"
+player1.show_state
+player2.show_state
+
+
+
 
--- lib/player.rb
+++ lib/player.rb
@@ -16,7 +16,21 @@ class Player
         @life_points = life_points - damage
         if @life_points <= 0
           puts "Le joueur #{@name} a été tué !"
+        @life_points = 0  # evite que le score descende en négatif
         end
     end
+
+    def attacks(player)
+        puts "#{@name} attaque #{player.name}"
+        damage = compute_damage
+        player.gets_damage(damage)
+        puts "Il lui inflige #{damage} points de dommages"
+      end
+    
+      
+    
+      def compute_damage
+        rand(1..6) 
+      end
 
 end
